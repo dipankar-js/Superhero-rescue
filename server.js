b6d6512cd0dccfde86da0cca33d2e2b2c91b4293
Dipankar498 <barman498@gmail.com>
2020-06-28 18:26:59 +0530

    done: route setup

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+// Routes files
+const mainRoute = require('./routes/main');
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Mount Routes
+app.use('/api/superhero', mainRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
